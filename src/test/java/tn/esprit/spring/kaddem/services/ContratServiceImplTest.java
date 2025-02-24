10b5b7c8f5e475fee1b26f8bb9d24e4928e4a14b
Slouumaa <abdessalem.chaouch@esprit.tn>
2025-02-24 12:24:22 +0100

    firstOne

---
+++ src/test/java/tn/esprit/spring/kaddem/services/ContratServiceImplTest.java
@@ -0,0 +1,44 @@
+package tn.esprit.spring.kaddem.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContratServiceImplTest {
+
+    @Test
+    void addContrat() {
+    }
+
+    @Test
+    void retrieveAllContrats() {
+    }
+
+    @Test
+    void updateContrat() {
+    }
+
+    @Test
+    void retrieveContrat() {
+    }
+
+    @Test
+    void removeContrat() {
+    }
+
+    @Test
+    void affectContratToEtudiant() {
+    }
+
+    @Test
+    void nbContratsValides() {
+    }
+
+    @Test
+    void retrieveAndUpdateStatusContrat() {
+    }
+
+    @Test
+    void getChiffreAffaireEntreDeuxDates() {
+    }
+}
