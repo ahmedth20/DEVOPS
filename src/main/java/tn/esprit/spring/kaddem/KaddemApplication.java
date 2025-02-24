d891d2b583646e0a58dc38791f89f73a8b6929a0
YOSRcharek <yosryassoura31@gmail.com>
2025-02-24 15:02:08 +0100

    prb in pom

--- src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
+++ src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
@@ -2,11 +2,12 @@ package tn.esprit.spring.kaddem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class KaddemApplication {
+public class KaddemApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(KaddemApplication.class, args);
