0be374ee1cab7bbfdef0d596c07d1d7699ee69de
YOSRcharek <94958031+YOSRcharek@users.noreply.github.com>
2025-02-26 23:48:22 +0100

    Update KaddemApplicationTests.java

--- src/test/java/tn/esprit/spring/kaddem/KaddemApplicationTests.java
+++ src/test/java/tn/esprit/spring/kaddem/KaddemApplicationTests.java
@@ -3,11 +3,11 @@ package tn.esprit.spring.kaddem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+/*@SpringBootTest*/
 public class KaddemApplicationTests {
 
-    @Test
+ /*   @Test
     void contextLoads(){
 
-    }
+    }*/
 }
