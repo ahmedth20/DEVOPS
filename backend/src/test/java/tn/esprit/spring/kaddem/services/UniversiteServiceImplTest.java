8aef142f97c15dc37845dcedc3949a0b30aeb2b2
YOSRcharek <94958031+YOSRcharek@users.noreply.github.com>
2025-04-18 06:30:57 +0100

    Update UniversiteServiceImplTest.java

--- backend/src/test/java/tn/esprit/spring/kaddem/services/UniversiteServiceImplTest.java
+++ backend/src/test/java/tn/esprit/spring/kaddem/services/UniversiteServiceImplTest.java
@@ -29,7 +29,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @ActiveProfiles("test")
 @Transactional
-public class UniversiteServiceImplTest {
+ class UniversiteServiceImplTest {
 
     @Autowired
     UniversiteRepository universiteRepository;
