dbaaa2bd7771b38069f1b84dd9c9b28664c7434a
YOSRcharek <94958031+YOSRcharek@users.noreply.github.com>
2025-04-18 06:30:44 +0100

    Update KaddemApplicationTests.java

--- backend/src/test/java/tn/esprit/spring/kaddem/KaddemApplicationTests.java
+++ backend/src/test/java/tn/esprit/spring/kaddem/KaddemApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class KaddemApplicationTests {
+ class KaddemApplicationTests {
 
   @Test
   void contextLoads() {
